b46c8d369330517cf7b13bccc2c879e552ee1997
kmk0815 <m.koeppchen@protronic-gmbh.de>
2026-08-12 13:14:37 +0200

    Fix STM32WBA65I-DK1 BLE advertising and USART2 pin conflict.
    Raise SYSCLK/AHB5 to radio-capable clocks, disable SPI1 on PA12, and put the device name in the primary advertising data.

--- src/main.c
+++ src/main.c
@@ -55,12 +55,12 @@ static K_WORK_DEFINE(adv_work, adv_work_handler);
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_NUS_SRV_VAL),
+	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME,
+		sizeof(CONFIG_BT_DEVICE_NAME) - 1),
 };
 
 static const struct bt_data sd[] = {
-	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME,
-		sizeof(CONFIG_BT_DEVICE_NAME) - 1),
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_NUS_SRV_VAL),
 };
 
 /* UART ISR: drain RX FIFO into the UART->BLE ring buffer, feed the TX FIFO
@@ -158,7 +158,7 @@ static void adv_work_handler(struct k_work *work)
 {
 	ARG_UNUSED(work);
 
-	int err = bt_le_adv_start(BT_LE_ADV_CONN_FAST_2, ad, ARRAY_SIZE(ad),
+	int err = bt_le_adv_start(BT_LE_ADV_CONN_FAST_1, ad, ARRAY_SIZE(ad),
 				  sd, ARRAY_SIZE(sd));
 
 	if (err == -EALREADY) {
